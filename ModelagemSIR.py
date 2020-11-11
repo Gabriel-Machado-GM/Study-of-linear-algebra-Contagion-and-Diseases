d3690d8857ad2bf3f3a89173479a57890a1bdd55
Raphael Levy <raphael.levy147@gmail.com>
2020-11-11 10:46:07 -0300

    Fiz um arquivo pra gráficos com dados reais, sem simulações

--- ModelagemSIR.py
+++ ModelagemSIR.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 21 20:03:54 2020
-
-@author: Gabriel Machado e Raphael Levy
-
-Esse arquivo mostra o processo de desenvolvimento de um modelo
-epidemiológico SIR, explica as várias variáveis e para fins de 
-exemplificação há simulações no fim do arquivo.
-"""
-
 ##############################################################################
 
 # Importando as bibliotecas
@@ -86,22 +75,22 @@ plt.show()
 ######################    SIMULACOES     ####################################
 ##############################################################################
 
-# Simulacao numero 1.
+# Simulacao numero 1, uma simulacao basica do COVID no Brasil dia 21/10/2020.
 
-# Populacao inicial ficticia.
-N1 = 2000
+# Populacao no Brasil
+N1 = 212205021 
 
 # Numeros iniciais de infectados e recuperados.
-I1, R1 = 500, 0
+I1, R1 = 5276942, 4721593 
 
 # Suscetiveis a contaminacao.
-S1 = N1 - I1 - R1
+S1 = N1 - I1 - R1 
 
 # Taxas tecnicas.
-beta1, gamma1 = 0.2, 1./10 
+beta1, gamma1 = 0.93, 0.8/10 
 
 # Rede de tempo.
-t1 = np.linspace(0, 100, 100)
+t1 = np.linspace(0, 100, 100) 
 
 # Equacoes diferenciais do modelo SIR.
 def deriv(y1, t1, N1, beta1, gamma1):
@@ -121,12 +110,12 @@ S1, I1, R1 = ret1.T
 # Plotando a simulacao.
 fig1 = plt.figure(facecolor='w')
 ax = fig1.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t1, S1/1000, 'b', alpha=0.5, lw=2, label='Suscetível')
-ax.plot(t1, I1/1000, 'r', alpha=0.5, lw=2, label='Infectado')
-ax.plot(t1, R1/1000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
+ax.plot(t1, S1/10000000, 'b', alpha=0.5, lw=2, label='Suscetível')
+ax.plot(t1, I1/10000000, 'r', alpha=0.5, lw=2, label='Infectado')
+ax.plot(t1, R1/10000000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
 ax.set_xlabel('Tempo /dias')
-ax.set_ylabel('Número (1000s)')
-ax.set_ylim(0,1.75)
+ax.set_ylabel('Número (10.000.000s)')
+ax.set_ylim(0,25)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -138,22 +127,22 @@ plt.show()
 
 ##############################################################################
 
-# Simulacao numero 2 (Infeccao por piolho em uma escola).
+# Simulacao numero 2, uma simulacao basica do COVID no mundo dia 02/11/2020.
 
-# População inicial ficticia.
-N2 = 1000
+# Populacao no mundo
+N2 = 7822785840 
 
 # Numeros iniciais de infectados e recuperados.
-I2, R2 = 200, 0
+I2, R2 = 46643798, 31156914
 
-# Suscetíveis a contaminação.
+# Suscetiveis a contaminacao.
 S2 = N2 - I2 - R2
 
 # Taxas tecnicas.
-beta2, gamma2 = 0.03, 2/10 
+beta2, gamma2 = 0.73, 0.64/10 
 
 # Rede de tempo.
-t2 = np.linspace(0, 50, 50)
+t2 = np.linspace(0, 100, 100) 
 
 # Equacoes diferenciais do modelo SIR.
 def deriv(y2, t2, N2, beta2, gamma2):
@@ -173,12 +162,12 @@ S2, I2, R2 = ret2.T
 # Plotando a simulacao.
 fig2 = plt.figure(facecolor='w')
 ax = fig2.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t2, S2/1000, 'b', alpha=0.5, lw=2, label='Suscetível')
-ax.plot(t2, I2/1000, 'r', alpha=0.5, lw=2, label='Infectado')
-ax.plot(t2, R2/1000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
+ax.plot(t2, S2/100000000, 'b', alpha=0.5, lw=2, label='Suscetível')
+ax.plot(t2, I2/100000000, 'r', alpha=0.5, lw=2, label='Infectado')
+ax.plot(t2, R2/100000000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
 ax.set_xlabel('Tempo /dias')
-ax.set_ylabel('Número (1000s)')
-ax.set_ylim(0,1)
+ax.set_ylabel('Número (100.000.000s)')
+ax.set_ylim(0,80)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -190,19 +179,19 @@ plt.show()
 
 ##############################################################################
 
-# Simulacao nUmero 3 (Infeccao por piolho em uma escola).
+# Simulacao numero 3, uma simulacao basica do COVID no mundo dia 02/11/2020 com a quarentena obedecida
 
-# PopulaCAo inicial fictIcia.
-N3 = 100
+# Populacao inicial ficticia.
+N3 = 7822785840 
 
-# NUmeros iniciais de infectados e recuperados.
-I3, R3 = 2, 0
+# Numeros iniciais de infectados e recuperados.
+I3, R3 = 46643798, 31156914
 
 # Suscetiveis a contaminacao.
 S3 = N3 - I3 - R3
 
 # Taxas tecnicas.
-beta3, gamma3 = 0.3, 0.2/10 
+beta3, gamma3 = 0.2, 0.64/10 
 
 # Rede de tempo.
 t3 = np.linspace(0, 150, 150)
@@ -225,12 +214,12 @@ S3, I3, R3 = ret3.T
 # Plotando a simulacao.
 fig3 = plt.figure(facecolor='w')
 ax = fig3.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t3, S3/100, 'b', alpha=0.5, lw=2, label='Suscetível')
-ax.plot(t3, I3/100, 'r', alpha=0.5, lw=2, label='Infectado')
-ax.plot(t3, R3/100, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
+ax.plot(t3, S3/100000000, 'b', alpha=0.5, lw=2, label='Suscetível')
+ax.plot(t3, I3/100000000, 'r', alpha=0.5, lw=2, label='Infectado')
+ax.plot(t3, R3/100000000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
 ax.set_xlabel('Tempo /dias')
-ax.set_ylabel('Número (100s)')
-ax.set_ylim(0,1)
+ax.set_ylabel('Número (100.000.000ss)')
+ax.set_ylim(0,80)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -242,22 +231,22 @@ plt.show()
 
 ##############################################################################
 
-# Simulacao numero 4, uma simulacao basica do COVID no Brasil dia 21/10/2020.
+# Simulacao numero 4, uma simulacao de Hepatite A em um hospital entre 2007 e 2008
 
-# Populacao no Brasil
-N4 = 212205021 
+# Populacao inicial
+N4 = 1630
 
 # Numeros iniciais de infectados e recuperados.
-I4, R4 = 5276942, 4721593 
+I4, R4 = 1396, 0
 
 # Suscetiveis a contaminacao.
-S4 = N4 - I4 - R4 
+S4 = N4 - I4 - R4
 
 # Taxas tecnicas.
-beta4, gamma4 = 0.93, 0.8/10 
+beta4, gamma4 = 0.62, 0.81/10 
 
 # Rede de tempo.
-t4 = np.linspace(0, 100, 100) 
+t4 = np.linspace(0, 50, 50)
 
 # Equacoes diferenciais do modelo SIR.
 def deriv(y4, t4, N4, beta4, gamma4):
@@ -277,12 +266,12 @@ S4, I4, R4 = ret4.T
 # Plotando a simulacao.
 fig4 = plt.figure(facecolor='w')
 ax = fig4.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t4, S4/10000000, 'b', alpha=0.5, lw=2, label='Suscetível')
-ax.plot(t4, I4/10000000, 'r', alpha=0.5, lw=2, label='Infectado')
-ax.plot(t4, R4/10000000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
+ax.plot(t4, S4/100, 'b', alpha=0.5, lw=2, label='Suscetível')
+ax.plot(t4, I4/100, 'r', alpha=0.5, lw=2, label='Infectado')
+ax.plot(t4, R4/100, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
 ax.set_xlabel('Tempo /dias')
-ax.set_ylabel('Número (10.000.000s)')
-ax.set_ylim(0,25)
+ax.set_ylabel('Número (100s)')
+ax.set_ylim(0,16)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -294,22 +283,22 @@ plt.show()
 
 ##############################################################################
 
-# Simulacao numero 5, uma simulacao basica do COVID no mundo dia 02/11/2020.
+# Simulacao numero 5, uma simulacao de dengue no sudoeste da Bahia em 2012
 
-# Populacao no mundo
-N5 = 7822785840 
+# Populacao na regiao
+N5 = 225530
 
 # Numeros iniciais de infectados e recuperados.
-I5, R5 = 46643798, 31156914
+I5, R5 = 166521, 0
 
 # Suscetiveis a contaminacao.
 S5 = N5 - I5 - R5 
 
 # Taxas tecnicas.
-beta5, gamma5 = 0.73, 0.64/10 
+beta5, gamma5 = 0.75, 0.2/10 
 
 # Rede de tempo.
-t5 = np.linspace(0, 100, 100) 
+t5 = np.linspace(0, 365, 365) 
 
 # Equacoes diferenciais do modelo SIR.
 def deriv(y5, t5, N5, beta5, gamma5):
@@ -329,64 +318,12 @@ S5, I5, R5 = ret5.T
 # Plotando a simulacao.
 fig5 = plt.figure(facecolor='w')
 ax = fig5.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t5, S5/100000000, 'b', alpha=0.5, lw=2, label='Suscetível')
-ax.plot(t5, I5/100000000, 'r', alpha=0.5, lw=2, label='Infectado')
-ax.plot(t5, R5/100000000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
+ax.plot(t5, S5/1000, 'b', alpha=0.5, lw=2, label='Suscetível')
+ax.plot(t5, I5/1000, 'r', alpha=0.5, lw=2, label='Infectado')
+ax.plot(t5, R5/1000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
 ax.set_xlabel('Tempo /dias')
-ax.set_ylabel('Número (100.000.000s)')
-ax.set_ylim(0,80)
-ax.yaxis.set_tick_params(length=0)
-ax.xaxis.set_tick_params(length=0)
-ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-legend = ax.legend()
-legend.get_frame().set_alpha(0.5)
-for spine in ('top', 'right', 'bottom', 'left'):
-    ax.spines[spine].set_visible(False)
-plt.show()
-
-##############################################################################
-
-# Simulacao numero 6, uma simulacao basica do COVID no mundo dia 02/11/2020 com a quarentena obedecida
-
-# Populacao inicial ficticia.
-N6 = 7822785840 
-
-# Numeros iniciais de infectados e recuperados.
-I6, R6 = 46643798, 31156914
-
-# Suscetiveis a contaminacao.
-S6 = N6 - I6 - R6
-
-# Taxas tecnicas.
-beta6, gamma6 = 0.2, 0.64/10 
-
-# Rede de tempo.
-t6 = np.linspace(0, 150, 150)
-
-# Equacoes diferenciais do modelo SIR.
-def deriv(y6, t6, N6, beta6, gamma6):
-    S6, I6, R6 = y6
-    dSdt6 = -beta6 * S6 * I6 / N6
-    dIdt6 = beta6 * S6 * I6 / N6 - gamma6 * I6
-    dRdt6 = gamma6 * I6
-    return dSdt6, dIdt6, dRdt6
-
-# Condicoes inicias do vetor.
-y6 = S6, I6, R6
-
-# Integracao do modelo de equacoes diferenciais do SIR na rede de tempo(t).
-ret6 = odeint(deriv, y6, t6, args=(N6, beta6, gamma6))
-S6, I6, R6 = ret6.T
-
-# Plotando a simulacao.
-fig6 = plt.figure(facecolor='w')
-ax = fig6.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t6, S6/100000000, 'b', alpha=0.5, lw=2, label='Suscetível')
-ax.plot(t6, I6/100000000, 'r', alpha=0.5, lw=2, label='Infectado')
-ax.plot(t6, R6/100000000, 'g', alpha=0.5, lw=2, label='Recuperado e imune')
-ax.set_xlabel('Tempo /dias')
-ax.set_ylabel('Número (100.000.000ss)')
-ax.set_ylim(0,80)
+ax.set_ylabel('Número (1000s)')
+ax.set_ylim(0,250)
 ax.yaxis.set_tick_params(length=0)
 ax.xaxis.set_tick_params(length=0)
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -414,5 +351,7 @@ plt.show()
     #https://covid19.who.int/table
     #https://ciis.fmrp.usp.br/covid19/epcalc/public/index.html
     #https://towardsdatascience.com/modelling-the-coronavirus-epidemic-spreading-in-a-city-with-python-babd14d82fa2
+    #http://scielo.iec.gov.br/scielo.php?script=sci_arttext&pid=S2176-62232010000200013
+    #https://www.scielo.br/scielo.php?script=sci_arttext&pid=S2179-84512014000300003
     
 ##############################################################################
